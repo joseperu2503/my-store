61b36a939a82fee4657b96fc1d96c60e43e09124
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-11-28 15:59:50 -0500

    Routing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('My App');
+//-------------------------------------------
+const express = require('express')
+const app = express()
+const port = 3000
+
+//------Routing-------------------------------
+app.get('/', (req,res) => {
+  res.send('hola mi server en express')
+})
+
+app.get('/nueva-ruta', (req,res) => {
+  res.send('hola soy una nueva ruta')
+})
+
+app.get('/products', (req,res) => {
+  res.json({
+    name: 'junior',
+    lastname: 'perez'
+  })
+})
+
+//-----------------------------------------
+app.listen(port,() => {
+  console.log('Mi port ' + port)
+})
