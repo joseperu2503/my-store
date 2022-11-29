a1db19c8cae8ca9ea8ca54d6f752590d35abffc3
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-11-29 00:00:22 -0500

    validacion de datos con joi

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const faker = require('faker')
 const ProductService = require('../services/product.service')
+const validatorHandler = require('../middlewares/validator.handler')
+const { createProductSchema, updateProductSchema, getProductSchema } = require('../schemas/product.schemas')
+
 
 const router = express.Router()
 const service = new ProductService()
@@ -14,7 +17,7 @@ router.get('/', async (req,res, next) => {
   }
 })
 
-router.get('/:id', async (req,res,next) => {
+router.get('/:id', validatorHandler(getProductSchema, 'params'), async (req,res,next) => {
   try {
     const {id} = req.params
     const product = await service.findOne(id)
@@ -24,24 +27,27 @@ router.get('/:id', async (req,res,next) => {
   }
 })
 
-router.post('/', async (req,res) => {
+router.post('/', validatorHandler(createProductSchema, 'body'), async (req,res) => {
   const body = req.body
   const newProduct = await service.create(body)
 
   res.status(201).json(newProduct)
 })
 
-router.put('/:id', async (req,res, next) => {
-  try {
-    const {id} = req.params
-    const body = req.body
-    const product = await service.update(id,body)
-    res.json(product)
-  } catch (error) {
-    next(error)
+router.put('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req,res, next) => {
+    try {
+      const {id} = req.params
+      const body = req.body
+      const product = await service.update(id,body)
+      res.json(product)
+    } catch (error) {
+      next(error)
+    }
   }
-
-})
+)
 
 router.delete('/:id', async (req,res) => {
   const {id} = req.params
